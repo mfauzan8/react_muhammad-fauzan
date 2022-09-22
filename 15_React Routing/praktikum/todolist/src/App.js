010329bf4f6462711fbe6287acfc379cac39747a
mfauzan8 <kerabatfauzan@gmail.com>
2022-09-22 17:43:19 +0700

    Feat: Add Page Not Found

--- 15_React Routing/praktikum/todolist/src/App.js
+++ 15_React Routing/praktikum/todolist/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./Component/About";
 import AboutTheApp from "./Component/AboutTheApp";
 import AboutAuthor from "./Component/AboutAuthor";
+import NotFoundPage from "./Component/NotFoundPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/about" exact element={<About />} />
       <Route path="/about/about-app" exact element={<AboutTheApp />} />
       <Route path="/about/about-author" exact element={<AboutAuthor />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ 15_React Routing/praktikum/todolist/src/Component/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div className="notFound">
+      ERROR
+      <span>404</span>
+      <h2>Page Not Found</h2>
+    </div>
+  );
+};
+export default NotFoundPage;
